af5d9ed9a75a81844878b8c7fd5c5bf84b359430
Dumisani Sithole <dusithol@c5r6s1.wethinkcode.co.za>
2020-07-13 11:52:31 +0200

    ex02

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -1,14 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dusithol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 11:48:51 by dusithol          #+#    #+#             */
+/*   Updated: 2020/07/13 11:48:55 by dusithol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	ft_putchar(char c);
 
-void	ft_print_reverse_alphabet(void) {
+
+
+void	ft_print_numbers(void)
+{
 	char	start;
 	char	end;
 
 	start = '0';
 	end = '9';
-	while (start <= end) {
+	while (start <= end)
+    {
 		ft_putchar(start);
 		start++;
 	}
 }
+
+
