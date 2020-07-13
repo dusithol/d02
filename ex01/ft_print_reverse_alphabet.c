8a37785ef37f67a328fcaf12590af7721ba1113f
Dumisani Sithole <dusithol@c5r6s1.wethinkcode.co.za>
2020-07-13 11:51:17 +0200

    ex01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,14 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dusithol <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 11:42:26 by dusithol          #+#    #+#             */
+/*   Updated: 2020/07/13 11:42:31 by dusithol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	ft_putchar(char c);
 
-void	ft_print_reverse_alphabet(void) {
+
+void	ft_print_reverse_alphabet(void)
+{
 	char	start;
 	char	end;
 
 	start = 'z';
 	end = 'a';
-	while (start => end) {
+	while (start >= end)
+   	{
 		ft_putchar(start);
 		start--;
 	}
 }
+
+
